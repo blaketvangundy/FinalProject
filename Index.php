d4d1a96257dfad2745585b7a50daffeea8947af3
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-04 12:34:36 -0600

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,9 +2,8 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<div display: flex; justify-content: center; align-items: center;>
+
     <h1>Final Project</h1>
-</div>
    
 <?php
 include "view-footer.php";
